bf5e3b3b63262a34576a89758313c54665227f24
malev.dg <malev.dg@outlook.com>
2022-03-09 08:20:20 -0500

    sprint 2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import App from './components/App.js';
 
-document.getElementById('root').appendChild(App());
+/*document.getElementById('root').appendChild(App());*/
+
+let playPause = document.getElementById("musicStart")
+playPause.addEventListener("click",() => {
+  let count = 0;
+
+  function playPause() {
+    if (count==0){
+      count=1;
+      audio.play();
+    }else{
+      count=0;
+      audio.pause();
+    }
+  }
